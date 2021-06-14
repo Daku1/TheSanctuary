4abecece34a151997af2c5d2a5580fe12679026d
Daku1 <karthikbhat1225@gmail.com>
2021-06-14 13:27:47 +0530

    completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import './Footer.css'
+import { FaLinkedin, FaInstagram } from 'react-icons/fa'
 
-import { FaFacebook, FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa'
 function Footer() {
     return (
         <div className="Footer-wrapper">
             <ul className="ssicons">
                 <li className="icons">
-                    <FaFacebook />
+                    <a href="https://www.linkedin.com/in/mkarthikeya/">
+                        <FaLinkedin />
+                    </a>
                 </li>
                 <li className="icons">
-                    <FaLinkedin />
-                </li>
-                <li className="icons">
-                    <FaInstagram />
-                </li>
-                <li className="icons">
-                    <FaTwitter />
+                    <a href="https://www.instagram.com/karthik_bht/">
+                        <FaInstagram />
+                    </a>
                 </li>
             </ul>
             <p>NoCopyrights @2021 | website By Karthikeya</p>
